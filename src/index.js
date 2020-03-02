34cf801c040b597140abea215092d53fd227fbc4
Joao Lucas <jlmelosouza18@gmail.com>
2020-03-01 22:55:44 -0300

    feat: card

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from './components/Header';
 import Tabs from './components/Tabs';
-import { SafeAreaView, Container } from './styles';
+import {
+  SafeAreaView,
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Index() {
   return (
@@ -12,6 +24,26 @@ export default function Index() {
       <SafeAreaView>
         <Container>
           <Header />
+
+          <Content>
+            <Card>
+              <CardHeader>
+                <Icon name="attach-money" size={28} color="#666" />
+                <Icon name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                <Title>Saldo disponível</Title>
+                <Description>R$ 197.611,65</Description>
+              </CardContent>
+              <CardFooter>
+                <Annotation>
+                  Transferência de R$ 2.542,97 recebida de Diego Schell
+                  Fernandes
+                </Annotation>
+              </CardFooter>
+            </Card>
+          </Content>
+
           <Tabs />
         </Container>
       </SafeAreaView>
--- src/styles.js
+++ src/styles.js
@@ -5,6 +5,57 @@ export const SafeAreaView = styled.SafeAreaView`
   background: #8b10ae;
 `;
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 400px;
+  z-index: 5;
+`;
+
+export const Card = styled.View`
+  background: #fff;
+  margin: 0 20px;
+  border-radius: 4px;
+  height: 100%;
+  position: absolute;
+  top: 380px;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
+
+export const Description = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
+
+export const CardFooter = styled.View`
+  padding: 30px;
+  background: #eee;
+  border-radius: 4px;
+`;
+
+export const Annotation = styled.Text`
+  font-size: 12px;
+  color: #333;
 `;
