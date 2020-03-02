dbbf9150fc792bad9d4fcb04bd3d744bb3ab9c1f
Joao Lucas <jlmelosouza18@gmail.com>
2020-03-01 22:38:38 -0300

    feat: tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { Container, TabsContainer, TabItem, TabText } from './styles';
 
 export default function Tabs() {
-  return <View />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#fff" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#fff" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear Cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 import Header from './components/Header';
+import Tabs from './components/Tabs';
 import { SafeAreaView, Container } from './styles';
 
 export default function Index() {
@@ -11,6 +12,7 @@ export default function Index() {
       <SafeAreaView>
         <Container>
           <Header />
+          <Tabs />
         </Container>
       </SafeAreaView>
     </>
